189b0dc9897dcaafe36f54e253c0686ebc51c633
77jodie77 <44469498+77jodie77@users.noreply.github.com>
2019-02-28 19:53:45 +0000

    read from file

--- Tester.java
+++ Tester.java
@@ -1,4 +1,99 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Tester {
+public class Main {
+	private int noImages;
+	private String[] tags;
+	private int noTags;
+	private Image tempVertical;
+	private int noOfSlides;
+	
+	
+
+
+	private boolean vertical;
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		
+		
+		main.readInput();
+		
+		Sets[] sets = new Sets[(main.getNoImages())/8];
+
+
+	}
+	
+	public int getNoImages() {
+		return noImages;
+	}
+	
+	
+	public void readInput()
+	{
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+		
+		try {
+			fileReader = new FileReader("a_example.txt");
+			
+			
+			bufferedReader = new BufferedReader(fileReader);
+			
+			String nextLine = bufferedReader.readLine();
+			
+			
+			noImages = Integer.parseInt(nextLine);
+			
+			System.out.println("Number of images: " + noImages);
+			nextLine = bufferedReader.readLine();
+			while (nextLine != null)
+			{
+				
+				String[] parts = nextLine.split(" ");
+				if(parts[0].equals("V"))
+				{
+					vertical = true;
+					System.out.println("vertical");
+				}
+				else
+				{
+					vertical = false;
+					System.out.println("horizontal");
+				}
+				
+				noTags = Integer.parseInt(parts[1]);
+				System.out.println("Number of tags: " + noTags);
+				tags = new String[noTags];
+				for(int i = 0; i<noTags; i++)
+				{
+					
+					tags[i] = parts[i+2];
+					System.out.println("tag " + (i+1) + ": " + tags[i]);
+				}
+				Image image = new Image(noTags, tags, vertical);
+				
+				nextLine = bufferedReader.readLine();
+				
+			}
+			
+			
+			bufferedReader.close();
+			
+		} catch (IOException error)
+		{
+			System.out.println("Error reading from file: " + error);
+		}
+		
+	}
+	
+	
 
 }
