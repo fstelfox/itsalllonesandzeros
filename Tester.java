886a4fd9f727c9a973937ef92afcc1b0794841cc
fstelfox <44262632+fstelfox@users.noreply.github.com>
2019-02-28 20:35:32 +0000

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -106,5 +106,21 @@ public class Tester {
 			Slide slide = new Slide(tempVertical, img);
 		}
 	}
+	
+	public void output(Sets collection[]) throws IOException {
+	BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
+	for(int i=0; i < collection.length; i++) {
+		Sets temp = collection[i];
+		Slides[] slides = temp.getSlides();
+		for(int x = 0; x < 8; x++) {
+			//True when slide is horizontal, e.g one image per slide
+			if(!slides[x].getPic().getVector()) {
+				writer.wrtie(slides[x].getPic[0].getIndex());
+			}else {
+				writer.write(slides[x].getPic[0].getIndex() + slides[x].getPic[1].getIndex());
+				}
+			}
+		}
+	}
 
 }
