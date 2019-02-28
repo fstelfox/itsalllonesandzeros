7ffdb31aaca14482cf3c9b1a8970717d570e18c2
ajholligan <33227000+ajholligan@users.noreply.github.com>
2019-02-28 18:50:47 +0000

    fixed

--- Slide.java
+++ Slide.java
@@ -1,4 +1,61 @@
 
 public class Slide {
 
+	private Image[] pic;
+	private boolean verticle;
+	private int noTags;
+	private String[] tags;
+	
+	
+	/**
+	 * @return the pic
+	 */
+	public Image[] getPic() {
+		return pic;
+	}
+	/**
+	 * @param pic the pic to set
+	 */
+	public void setPic(Image[] pic) {
+		this.pic = pic;
+	}
+	/**
+	 * @return the verticle
+	 */
+	public boolean isVerticle() {
+		return verticle;
+	}
+	/**
+	 * @param verticle the verticle to set
+	 */
+	public void setVerticle(boolean verticle) {
+		this.verticle = verticle;
+	}
+	/**
+	 * @return the noTags
+	 */
+	public int getNoTags() {
+		return noTags;
+	}
+	/**
+	 * @param noTags the noTags to set
+	 */
+	public void setNoTags(int noTags) {
+		this.noTags = noTags;
+	}
+	/**
+	 * @return the tags
+	 */
+	public String[] getTags() {
+		return tags;
+	}
+	/**
+	 * @param tags the tags to set
+	 */
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+	
+	
+	
 }
