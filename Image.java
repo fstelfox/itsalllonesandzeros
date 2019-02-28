283fc1af1fd4d3da39f30d3a8ae28a861d224166
ajholligan <33227000+ajholligan@users.noreply.github.com>
2019-02-28 18:14:05 +0000

    Update Image.java

--- Image.java
+++ Image.java
@@ -1,4 +1,59 @@
 
 public class Image {
+	
+
+	private int numTags;
+	private String[] tags;
+	private boolean vertical;
+	
+	public Image(int numTags, String[] tags, boolean vertical)
+	{
+		this.numTags = numTags;
+		this.tags = tags;
+		this.vertical = vertical;
+	}
+
+	/**
+	 * @return the numTags
+	 */
+	public int getNumTags() {
+		return numTags;
+	}
+
+	/**
+	 * @param numTags the numTags to set
+	 */
+	public void setNumTags(int numTags) {
+		this.numTags = numTags;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public String[] getTags() {
+		return tags;
+	}
+
+	/**
+	 * @param tags the tags to set
+	 */
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+
+	/**
+	 * @return the vertical
+	 */
+	public boolean isVertical() {
+		return vertical;
+	}
+
+	/**
+	 * @param vertical the vertical to set
+	 */
+	public void setVertical(boolean vertical) {
+		this.vertical = vertical;
+	}
 
 }
+
